07532a48e44f6504221f1dbcb22af84c1b73be84
chienchih <chien@xiaoxianroude-MacBook-Pro.local>
2019-09-04 17:26:12 -0700

    finished part1: stack&queue. However, I have not removed the Object.keys code structure for  stack.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,42 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  //variable tracks down the index of the object
+  var index =0;
+
+  var first = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+     //declare a variable for the number of keys in storage
+    var keys = Object.keys(storage);
+    //add new value into storage
+    if(keys.length===0){
+      first = 0;
+      storage[index] = value;
+    }
+    else{
+      index += 1;
+      storage[index] = value;
+    }
   };
 
   someInstance.dequeue = function() {
+     //declare a variable for the number of keys in storage
+    //var keys = Object.keys(storage);
+    var theRemoved = storage[first];
+    delete storage[first];
+
+    //update the index of the first element of sotrage object everytime you dequeue
+    first +=1 ;
+    return theRemoved;
   };
 
   someInstance.size = function() {
+     //declare a variable for the number of keys in storage
+    var keys = Object.keys(storage);
+    return keys.length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,40 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //add new value into someInstance object by using storage.
+    //edge case if storage has nothing in it
+    var theSize = Object.keys(storage);
+    if(theSize.length===0){
+      storage[0] = value;
+      // console.log(storage[0]);
+    }
+    else{
+      storage[theSize.length] = value;
+      // console.log(storage[theSize.length-1]);
+    }
+
   };
 
   someInstance.pop = function() {
+    //declare a variable to represent the size of the storage
+    var theSize = Object.keys(storage);
+    //set a variable to represent the key/value that got deleted
+    var theRemoved;
+    //if the storage is empty when called pop, return 0
+    if(theSize.length===0){
+      return 0;
+    }
+    theRemoved = storage[theSize.length-1];
+    delete storage[theSize.length-1];
+    return theRemoved;
   };
 
   someInstance.size = function() {
+    //return the size of the someInstance
+    var theSize = Object.keys(storage);
+    // console.log('the size');
+    // console.log(theSize);
+    return theSize.length;
   };
 
   return someInstance;
