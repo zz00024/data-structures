91c7715b36b8561c8474fc35b830149f77bcd159
chienchih <chien@xiaoxianroude-MacBook-Pro.local>
2019-09-04 17:28:25 -0700

    fixing the eslint form

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,39 +5,38 @@ var Queue = function() {
   var storage = {};
 
   //variable tracks down the index of the object
-  var index =0;
+  var index = 0;
 
   var first = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-     //declare a variable for the number of keys in storage
+    //declare a variable for the number of keys in storage
     var keys = Object.keys(storage);
     //add new value into storage
-    if(keys.length===0){
+    if (keys.length === 0) {
       first = 0;
       storage[index] = value;
-    }
-    else{
+    } else {
       index += 1;
       storage[index] = value;
     }
   };
 
   someInstance.dequeue = function() {
-     //declare a variable for the number of keys in storage
+    //declare a variable for the number of keys in storage
     //var keys = Object.keys(storage);
     var theRemoved = storage[first];
     delete storage[first];
 
     //update the index of the first element of sotrage object everytime you dequeue
-    first +=1 ;
+    first += 1;
     return theRemoved;
   };
 
   someInstance.size = function() {
-     //declare a variable for the number of keys in storage
+    //declare a variable for the number of keys in storage
     var keys = Object.keys(storage);
     return keys.length;
   };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -9,11 +9,10 @@ var Stack = function() {
     //add new value into someInstance object by using storage.
     //edge case if storage has nothing in it
     var theSize = Object.keys(storage);
-    if(theSize.length===0){
+    if (theSize.length === 0) {
       storage[0] = value;
       // console.log(storage[0]);
-    }
-    else{
+    } else {
       storage[theSize.length] = value;
       // console.log(storage[theSize.length-1]);
     }
@@ -26,11 +25,11 @@ var Stack = function() {
     //set a variable to represent the key/value that got deleted
     var theRemoved;
     //if the storage is empty when called pop, return 0
-    if(theSize.length===0){
+    if (theSize.length === 0) {
       return 0;
     }
-    theRemoved = storage[theSize.length-1];
-    delete storage[theSize.length-1];
+    theRemoved = storage[theSize.length - 1];
+    delete storage[theSize.length - 1];
     return theRemoved;
   };
 
