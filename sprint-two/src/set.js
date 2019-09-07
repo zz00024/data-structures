7154a7cf8439924fef2f46b85c56aaa06154052e
zz00024 <zz00024@gmail.com>
2019-09-07 16:06:06 -0700

    set completed: use object instead of array

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage[item] = item;
+  }
 };
+//Time complexity: O(n)
 
 setPrototype.contains = function(item) {
+  return this._storage.hasOwnProperty(item);
 };
+//Time complexity: O(n)
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
+//Time complexity: Constant time
 
 /*
  * Complexity: What is the time complexity of the above functions?
