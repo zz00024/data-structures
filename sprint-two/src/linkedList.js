2f68cac7f280dab4c33b011dfc4705806c0025d8
zz00024 <zz00024@gmail.com>
2019-09-06 20:47:54 -0700

    linkedList completed: go back and refractor

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var current = new Node(value);
+
+    if (list.tail === null) {
+      list.head = current;
+      list.tail = current;
+    } else {
+      list.tail.next = current;
+      list.tail = current;
+    }
   };
 
   list.removeHead = function() {
+    //check if the list is empty
+    if (list.head === null) {
+      return;
+    }
+    var removed = list.head.value;
+    list.head = list.head.next;
+    return removed;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
